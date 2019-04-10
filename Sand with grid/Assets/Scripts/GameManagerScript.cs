82a522dd6992c3fc3af7470839310b25523f5a63
edv576 <edgarv576@gmail.com>
2019-04-10 23:42:46 +0200

    New version

---
+++ Sand with grid/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using Random = System.Random;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public List<Particle> particles;
+
+    public List<GameObject> particleGOlist;
+
+    public HashSet<int>[,,] ParticleGrid;
+
+    // Start is called before the first frame update
+    public float ParticleMass = 0.15f;
+    public float ParticleRadius = 0.5f;
+    public int ParticleCount = 10;
+    public static float FieldWidth;
+
+    //inverted inertia tensor
+    private Vector3 inv_InertiaTensor;
+    private Random rng = new Random();
+
+    void Start()
+    {
+        particles = new List<Particle>();
+        FieldWidth = 2 * GameObject.Find("Floor").GetComponent<BoxCollider>().bounds.extents.x;
+
+        for (int l = 0; l < 3; l++)
+            for (int i = 0; i < ParticleCount; i++)
+            {
+                float x = (FieldWidth - 1) * (float)rng.NextDouble() - (FieldWidth - 1) / 2f;
+                float y = 5f + 1.5f * l;
+                float z = (FieldWidth - 1) * (float)rng.NextDouble() - (FieldWidth - 1) / 2f;
+
+                //float x = -2f + i*0.5f;
+                //float y = 2f;
+                //float z = -2f + i*0.5f;
+
+
+                //  x = 0.12f * i;
+
+                var p = new Particle(ParticleRadius, ParticleMass, new Vector3(x, y, z));
+                particles.Add(p);
+
+                var GO = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                GO.transform.localPosition = particles[i].position;
+
+                //  GO.AddComponent<SphereCollider>();
+                GO.GetComponent<SphereCollider>().isTrigger = true;
+
+                //Rigidbody rbody = new Rigidbody { isKinematic = false, useGravity = false };
+
+                // GO.AddComponent<Rigidbody>();
+                // GO.GetComponent<Rigidbody>().isKinematic = false;
+                // GO.GetComponent<Rigidbody>().useGravity = false;
+
+                // GO.GetComponent<SphereCollider>().isTrigger = true;
+                GO.name = "Sphere " + p.Id;
+
+                particleGOlist.Add(GO);
+
+
+            }
+
+        CreateParticleGrid();
+
+    }
+
+    void CreateParticleGrid()
+    {
+        float cellWidth = ParticleRadius * 2;
+        float minX = -FieldWidth;
+        float maxX = FieldWidth;
+        int maxY = 4 * (int)particles.Max(o => o.position.y); //4x the max y
+        float minZ = -FieldWidth;
+        float maxZ = FieldWidth;
+
+        int xzVoxels = (int)Math.Ceiling((maxX - minX) / cellWidth);
+        int yvoxels = (int)Math.Ceiling(maxY / cellWidth);
+
+        //ParticleGrid = new HashSet<int>[xzVoxels, yvoxels, xzVoxels];
+        ParticleGrid = new HashSet<int>[xzVoxels, yvoxels, xzVoxels];
+
+        for (int x = 0; x < xzVoxels; x++)
+            for (int y = 0; y < yvoxels; y++)
+                for (int z = 0; z < xzVoxels; z++)
+                    ParticleGrid[x, y, z] = new HashSet<int>();
+
+        InitGrid();
+
+    }
+
+    private void InitGrid()
+    {
+        for (int i = 0; i < ParticleCount; i++)
+        {
+            var p = particles[i];
+            float x = p.position.x;
+            float y = p.position.y;
+            float z = p.position.z;
+
+            //int xi = (int)(x / cellWidth);
+            //int yi = (int)(y / cellWidth);
+            //int zi = (int)(z / cellWidth);
+
+            Vector3Int idx = p.GetVoxelIdx();
+
+            try
+            {
+                ParticleGrid[idx.x, idx.y, idx.z].Add(p.Id);
+            }
+            catch (Exception e)
+            {
+                int asdsa = 1;
+
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //   float dt = Time.deltaTime;
+        //  float ratio = Time.deltaTime / 0.003f;
+
+        float deltaTime = 0.003f; //paper uses 0.3 ms, then scale it to unity's scale r=0.5
+
+        Debug.Log($"dt: {deltaTime}");
+        foreach (var particle in particles)
+        {
+            var prevVoxIdx = particle.GetVoxelIdx();
+            var prevpos = particle.position;
+            particle.Update(particles);
+
+
+
+
+
+            // float scale = 1f;
+            //semi-implicit euler
+            particle.COMVelocity += deltaTime * (particle.Force_l / particle.mass);
+            particle.position += deltaTime * particle.COMVelocity;
+            var newVoxel = particle.GetVoxelIdx();
+
+            if (newVoxel != prevVoxIdx)
+            {
+                int id = particle.Id;
+                try
+                {
+                    ParticleGrid[prevVoxIdx.x, prevVoxIdx.y, prevVoxIdx.z].Remove(id);
+                    ParticleGrid[newVoxel.x, newVoxel.y, newVoxel.z].Add(id);
+                }
+                catch (Exception e)
+                {
+                    int assd = 1;
+                }
+            }
+
+
+
+
+
+
+            var inv_inertia = particle.inv_inertia;
+            var inertia = new Vector3(1.0f / inv_inertia.x, 1.0f / inv_inertia.y, 1.0f / inv_inertia.z);
+            GameObject p = particleGOlist[particle.Id];
+
+            //var angVel = particle.mass * Vector3.Cross(particle.AngVelocity,
+            //                 new Vector3(1.0f / inv_inertia.x, 1.0f / inv_inertia.y, 1.0f / inv_inertia.z));
+
+            particle.AngVelocity += deltaTime * (particle.Force_t / particle.mass);
+            var aV = particle.mass * Vector3.Cross(particle.AngVelocity, inertia);
+
+
+            Quaternion r = p.transform.rotation;
+            //  Vector3 aV = particle.AngVelocity;
+            Quaternion avq = new Quaternion(aV.x, aV.y, aV.z, 1);
+            float dt2 = 0.5f * deltaTime;
+            r = add(r, (scale(dt2, r)) * avq * r);
+
+            p.transform.rotation = r;
+
+            //angVelocityQua = new Quaternion(angVelocity.x, angVelocity.y, angVelocity.z, 0);
+
+
+        }
+
+        Quaternion scale(float a, Quaternion q)
+        {
+            return new Quaternion(q.x * a, q.y * a, q.z * a, q.w * a);
+        }
+
+        Quaternion add(Quaternion A, Quaternion B)
+        {
+            return new Quaternion(A.x + B.x, A.y + B.y, A.z + B.z, A.w + B.w);
+
+        }
+
+        for (int i = 0; i < particles.Count; i++)
+        {
+            try
+            {
+                particleGOlist[i].transform.localPosition = particles[i].position;
+                if (particles[i].position.y < 0)
+                {
+                    //particles[i].position.y = 0.5f;
+                    // particleGOlist[i].transform.localPosition = particles[i].position;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log("tset");
+                int asdas = 1;
+            }
+        }
+    }
+}
---
+++ Sand with grid/Assets/Scripts/Particle.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Particle
+{
+    public int Id = 0;
+
+    private static int newID = 0;   //only call once to increment id number
+
+    private GameObject manager;
+    private GameManagerScript managerScript;
+
+    private float radius;
+    public float mass;
+
+
+    public Vector3 position;
+    public Quaternion orientation;
+
+    public Vector3 COMVelocity;    //linear velocity
+    public Vector3 AngVelocity;
+    public Vector3 inv_inertia;
+
+    public Vector3 Force_l;
+    public Vector3 Force_t;
+    // public List<Particle> Particles;
+
+    public float Diameter => radius * 2;
+    public float Ratio => Time.deltaTime / 0.003f;
+
+    // private float kn => OGkn * Ratio;
+    // private float ys => OGys * Ratio;
+    // private float u => OGu * Ratio;
+    // private float ks => OGks * Ratio;
+
+    public Vector3Int GetVoxelIdx()
+    {
+        float maxx = managerScript.ParticleGrid.GetLength(0);
+        float maxy = managerScript.ParticleGrid.GetLength(1);
+        float maxz = managerScript.ParticleGrid.GetLength(2);
+
+        float x = position.x + maxx / 4;
+        float y = position.y + maxy / 4;
+        float z = position.z + maxz / 4;
+
+        if (x < 0) x = 0;
+        if (y < 0) y = 0;
+        if (z < 0) z = 0;
+
+        if (x > maxx) x = maxx - 1;
+        if (y > maxy) y = maxy - 1;
+        if (z > maxz) z = maxz - 1;
+
+        int xi = (int)(x /*/ radius * 2*/);
+        int yi = (int)(y /*/ radius * 2*/);
+        int zi = (int)(z /*/ radius * 2*/);
+        return new Vector3Int(xi, yi, zi);
+    }
+
+    float kn = 500f;
+    float ys = 10;
+    float u = 0.05f;
+    float ks = 20;
+
+
+
+    float alpha = 0.5f;
+    float beta = 1.5f;
+
+    public Particle(float r, float m, Vector3 pos)
+    {
+        Id = newID++;
+        position = pos;
+
+        radius = r;
+        mass = m;
+        //COMVelocity = linV;
+        //AngVelocity = angV;
+
+        float inertia = 1.0f / ((2.0f / 5.0f) * mass * radius * radius);
+        inv_inertia = new Vector3(inertia, inertia, inertia);
+        orientation = new Quaternion(1, 1, 1, 1);
+        manager = GameObject.Find("GameManager");
+        managerScript = manager.GetComponent<GameManagerScript>();
+        //   Particles = manager.GetComponent<GameManagerScript>().particles;
+    }
+
+    public void Update(List<Particle> particles, bool fromCollisionCall = false)
+    {
+
+        float ratio = Time.deltaTime / 0.003f;
+
+
+        Vector3 sumF_n = Vector3.zero;
+        Vector3 sumF_t = Vector3.zero;
+
+        Force_l = new Vector3(0, 0, 0);
+        Force_t = new Vector3(0, 0, 0);
+
+
+        //set constraint y = 0 as floor
+        // sumF_n = GetFloorForces(kn, ys, alpha, beta, sumF_n);
+        GetStaticCollisionForces(ref sumF_n, ref sumF_n, kn, ys, u, ks, alpha, beta);
+
+        //collision with other particles
+        GetParticleCollisionForces(particles, ref sumF_n, ref sumF_t);
+
+        //add gravity and sum all the forces into linear force
+        Vector3 gravity = new Vector3(0, -9.81f, 0) * mass;
+        Force_l += gravity;
+        Force_l += sumF_n;
+    }
+
+    public Vector3 CalcCollisionForce(Vector3 HitPoint, Vector3 hitNormal)
+    {
+        Vector3 sumF_n = Vector3.zero;
+
+        var distance = Vector3.Distance(HitPoint, position);
+
+        Vector3 relativeVelocity = COMVelocity;
+
+        float E = Mathf.Max(0, radius - distance);
+        float E2 = Vector3.Dot(relativeVelocity, hitNormal);
+
+        Vector3 F_n = -(kn * Mathf.Pow(E, beta) + ys * E2 * Mathf.Pow(E, alpha)) * hitNormal;
+
+        sumF_n += F_n;
+
+
+        return sumF_n;
+    }
+
+
+    private void GetStaticCollisionForces(ref Vector3 sumFN, ref Vector3 vector3,
+        float kn, float ys, float u, float ks, float alpha, float beta)
+    {
+        int mask = LayerMask.GetMask("CollisionObject");
+        var coll = Physics.OverlapSphere(position, radius, mask);
+        // foreach (var collider in coll) 
+
+        foreach (var collider in coll)
+        {
+            Debug.Log($"coll with {collider.gameObject.name}");
+            var hitP = collider.ClosestPointOnBounds(position);
+
+            var contactNormal = (hitP - position).normalized;
+            // if (hitP == position)
+
+            contactNormal = (collider.transform.position - position).normalized;
+
+            //  if (contactNormal == Vector3.zero) contactNormal = -Vector3.up;
+
+            var distance = Vector3.Distance(hitP, position);
+            //Vector3 contactNormal = new Vector3(0, -1, 0);
+            Vector3 relativeVelocity = COMVelocity;
+
+            float E = Mathf.Max(0, radius - distance);
+            float E2 = Vector3.Dot(relativeVelocity, contactNormal);
+
+            Vector3 F_n = -(kn * Mathf.Pow(E, beta) + ys * E2 * Mathf.Pow(E, alpha)) * contactNormal;
+
+            sumFN += F_n;
+
+        }
+
+
+
+        GetWallForces(ref sumFN);
+        GetFloorForces(ref sumFN);
+        //if (position.y < radius) position.y = radius;
+
+    }
+
+    public void GetWallForceX(ref Vector3 sumF_n)
+    {
+        float L = GameManagerScript.FieldWidth / 2f;
+        float left = position.x < 0 ? -1 : 1;
+        float wallX = left * L;
+        float distance = Mathf.Abs(position.x - wallX);
+
+        if (!(distance < radius)) return;
+        Debug.Log("WallX!");
+
+        Vector3 contactNormal = new Vector3(left * 1, 0, 0);
+        Vector3 relativeVelocity = COMVelocity;
+
+        float E = Mathf.Max(0, radius - distance);
+        float E2 = Vector3.Dot(relativeVelocity, contactNormal);
+
+        Vector3 F_n = -(kn * Mathf.Pow(E, beta) + ys * E2 * Mathf.Pow(E, alpha)) * contactNormal;
+
+        sumF_n += F_n;
+    }
+
+    public void GetWallForceZ(ref Vector3 sumF_n)
+    {
+        float L = GameManagerScript.FieldWidth / 2f;
+
+        float deep = position.z < 0 ? -1 : 1;
+        float wallX = deep * L;
+        float distance = Mathf.Abs(position.z - wallX);
+
+        if (!(distance < radius)) return;
+        Debug.Log("WallZ!");
+
+        Vector3 contactNormal = new Vector3(0, 0, deep * 1);
+        Vector3 relativeVelocity = COMVelocity;
+
+        float E = Mathf.Max(0, radius - distance);
+        float E2 = Vector3.Dot(relativeVelocity, contactNormal);
+
+        Vector3 F_n = -(kn * Mathf.Pow(E, beta) + ys * E2 * Mathf.Pow(E, alpha)) * contactNormal;
+
+        sumF_n += F_n;
+
+    }
+
+    public void GetWallForces(ref Vector3 sumF_n)
+    {
+        GetWallForceX(ref sumF_n);
+        GetWallForceZ(ref sumF_n);
+
+
+
+    }
+
+
+    public Vector3 GetFloorForces(ref Vector3 sumF_n)
+    {
+        if (position.y <= radius)
+        {
+            Debug.Log("y<0!");
+            var distance = Vector3.Distance(new Vector3(position.x, 0, position.z), position);
+            Vector3 contactNormal = new Vector3(0, -1, 0);
+            Vector3 relativeVelocity = COMVelocity;
+
+            //  float E = Mathf.Max(0, radius - distance);
+            float E = 0.5f - distance;
+            float E2 = Vector3.Dot(relativeVelocity, contactNormal);
+            float ys2 = 10;
+
+            Vector3 F_n1 = -(kn * Mathf.Pow(E, beta) + ys2 * E2 * Mathf.Pow(E, alpha)) * contactNormal;
+
+            sumF_n += F_n1;
+        }
+
+        return sumF_n;
+    }
+
+    private void GetParticleCollisionForces(List<Particle> particles, ref Vector3 sumF_n, ref Vector3 sumF_t)
+    {
+        var nearbyParticles = new List<int>();
+
+        Vector3Int myIdx = GetVoxelIdx();
+        int maxX = managerScript.ParticleGrid.GetLength(0);
+        int maxY = managerScript.ParticleGrid.GetLength(1);
+        int maxZ = managerScript.ParticleGrid.GetLength(2);
+
+        int startX = Math.Max(0, myIdx.x - 1);
+        int endX = Math.Min(maxX, myIdx.x + 1);
+
+        int startY = Math.Max(0, myIdx.y - 1);
+        int endY = Math.Min(maxY, myIdx.y + 1);
+
+        int startZ = Math.Max(0, myIdx.z - 1);
+        int endZ = Math.Min(maxZ, myIdx.z + 1);
+
+        for (int x = startX; x < endX; x++)
+            for (int y = startY; y < endY; y++)
+                for (int z = startZ; z < endZ; z++)
+                {
+                    HashSet<int> nb = managerScript.ParticleGrid[x, y, z];
+                    foreach (var nbId in nb)
+                    {
+                        nearbyParticles.Add(nbId);
+                    }
+                }
+
+
+
+
+
+        foreach (int idx in nearbyParticles)
+        {
+            Particle other = particles[idx];
+
+            if (other.Id == Id) continue;
+
+            Vector3 pos = other.position;
+            float d = Vector3.Distance(position, pos);
+
+            if (d <= radius && d > 0)
+            {
+                Vector3 contactNormal = Vector3.Normalize(pos - position);
+                contactNormal.Normalize();
+
+                //relative velocity
+                Vector3 V = (COMVelocity - other.COMVelocity).normalized;
+
+                //xi values from paper
+                float E = Mathf.Max(radius - d, 0);
+                float E2 = Vector3.Dot(V, contactNormal);
+
+                Vector3 tV = V - E2 * contactNormal;
+                Vector3 F_n = -(kn * Mathf.Pow(E, beta) + ys * E2 * Mathf.Pow(E, alpha)) * contactNormal;
+
+                Vector3 cross = Vector3.Cross(0.5f * contactNormal, tV).normalized;
+                Vector3 F_t = -Mathf.Min(u * F_n.magnitude, ks * tV.magnitude) * cross;
+
+                sumF_n += F_n;
+                sumF_t += F_t;
+
+            }
+        }
+    }
+
+
+}
